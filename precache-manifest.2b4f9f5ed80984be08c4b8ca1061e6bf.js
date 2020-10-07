c26e415f551ba28fe2401172c49ab55387a56c23
quocdung1999 <quocdungstrongsss@gmail.com>
2020-10-07 21:47:21 +0700

    Updates

---
+++ precache-manifest.2b4f9f5ed80984be08c4b8ca1061e6bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5811869fa8fcf7a1bf607c0198c19b52",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9a1b9228636d210c41fc",
+    "url": "/my-app/static/css/main.ff04cbb2.chunk.css"
+  },
+  {
+    "revision": "f71923f1c822833e3349",
+    "url": "/my-app/static/js/2.6c37b777.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.6c37b777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1b9228636d210c41fc",
+    "url": "/my-app/static/js/main.5ff4dafe.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
